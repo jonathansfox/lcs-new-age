973ea8115adf6d846bc4f425a002ce7319b4ad90
Jonathan S.Fox <jonathansfox@gmail.com>
2025-04-12 12:36:28 -0700

    Add knowledge skills to writing like the hint text says

--- lib/daily/activities/guardian.dart
+++ lib/daily/activities/guardian.dart
@@ -7,7 +7,13 @@ import 'package:lcs_new_age/utils/lcsrandom.dart';
 void doActivityWriteGuardian(List<Creature> people) {
   for (Creature p in people) {
     politics.addBackgroundInfluence(
-        View.issues.random, p.skillRoll(Skill.writing));
+        View.issues.random,
+        p.skillRoll(Skill.writing) +
+            p.skill(Skill.law) +
+            p.skill(Skill.science) +
+            p.skill(Skill.religion) +
+            p.skill(Skill.business) -
+            5);
     p.train(Skill.writing, 5);
   }
 }
@@ -15,7 +21,14 @@ void doActivityWriteGuardian(List<Creature> people) {
 void doActivityStreamGuardian(List<Creature> people) {
   for (Creature p in people) {
     politics.addBackgroundInfluence(
-        View.issues.random, 4 * p.skillRoll(Skill.persuasion));
+        View.issues.random,
+        4 *
+            (p.skillRoll(Skill.persuasion) +
+                p.skill(Skill.law) +
+                p.skill(Skill.science) +
+                p.skill(Skill.religion) +
+                p.skill(Skill.business) -
+                5));
     p.train(Skill.persuasion, 5);
   }
 }
